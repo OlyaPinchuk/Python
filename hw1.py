bda068e04742390f2213e8364fa0a37351db7afd
OlyaPinchuk <olha5959@gmail.com>
2021-02-22 21:31:03 +0200

    Add changes.

--- hw1.py
+++ hw1.py
@@ -13,52 +13,111 @@
 # 3) переделать первое задание под меню с помощью цикла
 # 4) вывести табличку умножения с помощью цикла while
 
-    # Забыл
-    # вам
-    # показать
-    # отрицание:
-    # в
-    # js
-    # у
-    # вас
-    # было:
-    # !true -> false
-    # в
-    # python:
-    # not True -> False
-    #
-    # так
-    # же
-    # вместо | | тут
-    # используется
-    # слово or, и
-    # вместо & & слово and
+# Забыл
+# вам
+# показать
+# отрицание:
+# в
+# js
+# у
+# вас
+# было:
+# !true -> false
+# в
+# python:
+# not True -> False
+#
+# так
+# же
+# вместо | | тут
+# используется
+# слово or, и
+# вместо & & слово and
+
+# реализовать записную книжку покупок:
+# - каждая запись должна содержать название покупки и ее цену
+# -сделать менюшку для работы с записной книжкой:
+#     '1) Создать запись'
+#     '2) Список все записей'
+#     '3) Общая сумма всех покупок'
+#     '4) Самая дорогая покупка'
+#     '5) Поиск по названию покупки'
+#     '9) Выход'
+# - можете придумать свои пункты
 
 # print('hello world')
 
 list1 = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
-# 1 print(min(list1))
+
+# for i in list1:
+#     if list1.count(i) > 1:
+#         while list1.count(i):
+#             list1.remove(i)
+# print(list1)
+
+# 1.1 print(min(list1))
 
 # list1.remove(2)
 # print(list1)
 # print(list1.count(22))
 # print(list1[3::4])
 
-# 2 ?????????????
-# for i in list1:
-#     if
+# 1.2 ?????????????
+listOfDuplicates = [];
+print(list1)
+count = 0
+for i in list1:
+    for k in list1:
+        if i == k:
+            # перевірка чи є ця пара вже в listOfDuplicates
+            listOfDuplicates.append({i: count})
+            print(i)
 
-# # 3
+print(listOfDuplicates)
+
+# # 1.3
 # for i in list1[::4]:
 #     list1[list1.index(i)] = 'x'
 #     print(i)
-print(list1)
+# print(list1)
+
+# 2)вывести на экран пустой квадрат из "*" сторона которого указана в переменой:
+# side = input('Enter side length:')
+# print('Side length:', side)
+# a = int(side)
+# i = 1
+#
+# while i <= a:
+#     if i == 1:
+#         print('*' * (a+2))
+#     print('*', ' ' * (a-2), '*')
+#     if i == a:
+#         print('*' * (a+2))
+#     i += 1
+
+# 3) переделать первое задание под меню с помощью цикла
 
 
-
-
-
-
-
-
-
+# a = '1 найти min число в листе'
+# b = '2 удалить все одинаковые значения'
+# c = '3 заменить каждое четвертое значение на "Х'
+# d = '4 вывести элемент листа, значение которого ближе всего к среднему арифметическому всех элементов этого же листа'
+#
+# menuList = [a, b, c, d]
+#
+# for i in menuList:
+#     print(i)
+# e = input('Choose an option:')
+# print('Your chosen task:', e)
+#
+# chosenTask = int(e)
+# print(menuList[int(e) - 1])
+#
+# if chosenTask == 1:
+#     print(min(list1))
+#
+# if chosenTask == 3:
+#     for i in list1[::4]:
+#         list1[list1.index(i)] = 'x'
+#     print(i)
+# print(list1)
