d8f7ee4f1015534c354a749c7972639ab2ec1083
OlyaPinchuk <olha5959@gmail.com>
2021-02-24 01:32:03 +0200

    Add changes.

---
+++ lesson1_code.py
@@ -0,0 +1,152 @@
+# # sdfjkhsdf
+# # """
+# # sdfs
+# # sddf
+# # sdf
+# # sdf
+# # """
+# # '''
+# # sdfsdf
+# # sdfsdf
+# # '''
+# # '''
+# #
+# # '''
+
+# print(1, 2, 3, 4, 6, sep='-', end=' Finish')
+# print()
+#
+# i = 3
+# f = 1.3
+# b = False  # True
+# s = "text"
+# n = None
+#
+# print(type(i))
+# print(type(f))
+# print(type(b))
+# print(type(n))
+#
+# a = b = c = 10
+#
+# print(a,b,c)
+
+# a = 3
+# b = 2
+# # a += 2
+# # a = a + 2
+# # print(a + b)
+# # print(a - b)
+# # print(a * b)
+# print(a // b)
+# print(a % b)
+# print(round(a / b))
+#
+# print(a ** b)
+
+# s = 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+# # print(s ** s)
+# i = int('1')
+# float()
+# bool()
+# str()
+# # num = input("Enter num: ")
+# print(type(i))
+# print(i)
+
+# # l = [1,2,3,4,5]
+# l = list()
+#
+# print(l)
+#
+#
+# l = [1, 2, 3, 4, 5, 6, 7, 8]
+# # print(l[-2])
+# l[7] = 9
+
+# del l[9]
+# print(l)
+# empty = []
+# l1 = [1, 2, 3, 4, 2]
+# l2 = l1
+#
+# l2[0] = 100
+# print(l1, l2)
+# l2 = [8, 9, 10]
+# l.append(1)
+# # l.append(2)
+# l.insert(30, 222)
+# # pop = l.pop()
+# pop = l.pop(8)
+# l.remove(2)
+# l.extend(l2)
+# print(l.index(2, 3))
+# l.reverse()
+# l.sort(reverse=True)
+# print(l)
+# print(l.count(3))
+# copy = l.copy()
+# print(copy)
+# l1 = [1, 2, 3, 4, 2, 3, 4, 5, 6, 7, 8]
+# print(l1[::3])
+# a = 2
+# b = 3
+# print(a < b)
+# print(a > b)
+# print(a <= b)
+# print(a >= b)
+# print(a == b)
+# print(a != b)
+
+# '', [], {}, 0, None -> False
+#
+# #
+# if a > b:
+#     print('True')
+#     print('block')
+# elif b>5:
+#     kjsdfh
+#
+#
+# print('end')
+
+# num = int(input("enter num:"))
+#
+# res = 'yes' if num > 5 else 'no'
+# print(res)
+# tuple
+
+# t = ('vasia', 32, False)
+
+# bool = False
+
+# dictionary = {}
+# dictionary = dict()
+# dictionary = {'name': 'Max', 'age': 13}
+# print(dictionary['name1'])
+# print(dictionary.get('name1', 'Karina'))
+# pop1 = dictionary.popitem()
+# # print(pop1)
+# d = dict([('age', 13),('name','Max'), ('status',False)])
+#
+# print(d)
+# dictionary.setdefault('name2',2222)
+# print(dictionary)
+# print(dictionary.items())
+
+# i = 5
+# while i > 5:
+#     i -= 1
+#     if i == 2:
+#         continue
+#     print(i)
+# else:
+#     print('Finish')
+# print(type(range(100)))
+# l1 = [5,8,1,3,8]
+#
+# for i in l1[::2]:
+#     print(i)
+
+# for k, v in dictionary.items():
+#     print(v)
